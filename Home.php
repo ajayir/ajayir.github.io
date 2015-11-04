7d184d7d7b23c62dbefa6b9d26245474b1514c76
ajayir <reubenajayilsus@gmail.com>
2015-11-04 10:02:44 -0600

    Create Home.php
    Build initial home page.

---
+++ Home.php
@@ -0,0 +1,71 @@
+		<title>Project: Blog</title>
+		<?php include("nav_bar.php")?>
+		
+        <h3>Latest Updates</h3>
+        <ul>
+			<li ><a class="cont" href="#me">About Reuben</a></li>
+            <li><a class="cont" href="#lop">Africa: The Land of Plenty</a></li>
+            <li><a class="cont" href="#TheJourney">The Journey of Infinity Miles</a></li>
+        </ul>
+		<div class="sidebar"><h4>Cool Stuff</h4>
+		<p id="p-sidebar">Here, I will be posting some cool links to and information about the latest technologies.
+		These posts will be emphasize on the latest techs emerging from <strong class="africa">Africa</strong> as a continent.
+		There is a great need for the awareness of these type of technology news in order to attract a great number of investor and 
+		entrepreneurs around the globe. Just as the western world needs foreign investors, so does <strong class="africa">Africa</strong> need 
+		them. The wrong perception is a killer.</p></div>
+        
+        <h2 id="me"><span class="heads-one">About </span> <span class="headings">Reuben</span></h2>
+        <h6>Posted: 07/23/2015</h6>
+		
+        <div id="par-one">
+		<p>I started college thinking I knew, career-wise, what my future ambition is; I thought wrong. 
+		The more computer science classes I took, the more I changed my decisions on what area of technology I would concentrate on. 
+		<span class="cis"><strong>Computer Information Systems</strong></span>, a broad major, is the major that best suits my indecisive nature towards my career.
+		I had browsed, like I was shopping, through the web for different professions in the CIS industry but to no avail.
+		Finally, like a flabbergasted kid who just got a surprise from his father, I realized that web development is actually what I am called to do! 
+		I currently enjoy it. I am still learning; of course, it is technology..duh!</p></div>
+        
+        <h2 id="lop"><span class="heads-one">Africa: </span> <span class="headings">The Land of Plenty</span></h2>
+        <h6>Posted: 07/23/2015</h6>
+        
+        <h4 class="single-story"><strong id="single-story2">The Single Story of Africa.</strong><div id="more-to-it">There is more to it..</div></h4>
+        
+        <div id="lop2"><img id="africa-image" src="https://upload.wikimedia.org/wikipedia/commons/9/93/Water_distribution_in_Horn_of_Africa.jpg"
+		alt = "Picture of a typical perception of Africa">
+        
+        <p>
+			<strong class="africa">Africa</strong>: The land truly flowing with milk and honey. 
+			<br><strong class="africa">Africa</strong>: The land of abundance.
+			<br><strong class="africa">Africa</strong>: The land with infinite natural resources. 
+			<br> Now, the question is why is the picture above an instance of what many in the western part of the world would instantly picture,
+			in their mind, when they hear "Africa"? 
+			<Strong class="africa">Africa</strong> is so rich that there should be no one soul living in penury.
+			Of course, there is corruption in its government. Of course, there are rural and poor environments. 
+			<span id="afric-africa">That single story of <strong class="africa">Africa</strong> is hurting it.</span> It has its beautiful sceneries as well; beautiful people, tasty foods,
+			interesting cultures and traditions, and breathe-taking architectures. <br><strong id="note">NOTE:</strong>
+			<strong class="africa">Africa</strong> is a continent housing over 53 countries.
+		</p>
+		</div>
+        
+        <h2 id="TheJourney"><span class="heads-one">The Journey of </span> <span class="headings">Infinite Miles</span></h2>
+        <h6>Posted: 07/23/2015</h6>
+        <img id="journey-image" src="https://www.kasandbox.org/programming-images/landscapes/mountains-in-hawaii.png"
+		alt = "Picture of a beautiful mountains depicting a journey of infinite miles">
+        <p id="my-journey">My journey started from my mother's womb....(to be continued)</p>
+		<p class="footer"><strong>CONTACT REUBEN:</strong> <br>reubenajayilsus@gmail.com</p>
+		<div class="form">
+	    <h4>Please fill in your info. for any suggestions and enquiries:</h4>
+	    <form action="message.php" method="post">
+			<label>First Name:</label>
+				<input type="text" name="first_name" maxlength="20" size="20" placeholder="Reuben"><br>
+	        <label>Last Name: </label>
+				<input type="text" name="last_name" maxlength="20" size="20" placeholder="Ajayi">
+	        <label>Suggestion & Enquiry:</label>
+				<textarea type="text" name="suggestions_enquiry" maxlength="1000" value="suggestions_enquiry" max-width="50">
+				</textarea>	
+			<input type = "submit" value = "Send">
+	    </form>
+		</div>
+	</div>
+    </body>
+</html>
